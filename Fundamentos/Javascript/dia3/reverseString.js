1050f4b6682a832ca1229ce7657258cb2f9588f8
matheus <mfaedebairros@gmail.com>
2022-10-04 20:13:47 -0300

    Revertendo strings com funções integradas

---
+++ Fundamentos/Javascript/dia3/reverseString.js
@@ -0,0 +1,8 @@
+// REVERTENDO STRINGS COM FUNÇÕES INTEGRADAS
+
+let reverseString = 'tryber'
+let splitstring = reverseString.split('');
+let reverseArray = splitstring.reverse();
+let joinArray = reverseArray.join('');
+console.log(joinArray);
+
