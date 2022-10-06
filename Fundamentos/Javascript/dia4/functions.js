296d6caa12c0719cddfa9c5780aa73dfc3fb526a
matheus <mfaedebairros@gmail.com>
2022-10-06 09:46:39 -0300

    Adiciona função menorValor() que retorna indice do menor valor

--- Fundamentos/Javascript/dia4/functions.js
+++ Fundamentos/Javascript/dia4/functions.js
@@ -21,5 +21,20 @@ function maiorValor(array) {
     }
     return indice;
 }
-const arrayTeste = [2, 3, 6, 7, 10, 1];
-console.log(maiorValor(arrayTeste));
+const arrayMaiorValor = [2, 3, 6, 7, 10, 1];
+console.log(maiorValor(arrayMaiorValor));
+
+function menorValor(array) {
+    let menor = array[0];
+    let indice = 0;
+    for (let i = 0; i < array.length; i += 1) {
+        if (array[i] < menor) {
+            menor = array[i]
+            indice = i;
+        }
+    }
+    return indice;
+}
+
+const arrayMenorValor = [2, 4, 6, 7, 10, 0, -3];
+console.log(menorValor(arrayMenorValor));
